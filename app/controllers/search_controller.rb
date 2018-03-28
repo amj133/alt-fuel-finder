5f77cc0a67816f1adc426aebae99dda2966ed19a
amj133 <amj@vt.edu>
2018-03-28 10:03:26 -0600

    add pry to gemfile, built out api call in search index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,17 @@
 class SearchController < ApplicationController
 
   def index
-    @conn = Faraday.new(url: "https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1") do |faraday|
-      faraday.headers["NREL-API-KEY"] = ENV['NREL-API-KEY']
-      faraday.adapter Faraday.default_adapter
-    end
+    binding.pry
+    params = {
+      "fuel_type" => "ELEC,LPG",
+      "api_key" => ENV['NREL-API-KEY'],
+      "location" => 80203,
+      "limit" => 10,
+      "radius" => 6
+    }
+    conn = Faraday.new(url: "https://developer.nrel.gov/")
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json")
+
   end
 
 end
