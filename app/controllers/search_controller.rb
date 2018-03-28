90734bd2f17f9848424220b25672883b35554d4d
amj133 <amj@vt.edu>
2018-03-28 09:52:41 -0600

    create @conn in search index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,12 @@
 class SearchController < ApplicationController
 
   def index
-
+    @conn = Faraday.new(url: "https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1") do |faraday|
+      faraday.headers["NREL-API-KEY"] = ENV['NREL-API-KEY']
+      faraday.adapter Faraday.default_adapter
+    end
   end
 
 end
+
+# https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/nearest/
