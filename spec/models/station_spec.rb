789e24b749bc24b1642031f5f910c8d9b4d5646c
amj133 <amj@vt.edu>
2018-03-28 10:28:07 -0600

    station_spec passing

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 describe Station do
   let(:attrs) {
-    station_name: "Bobs",
-    street_address: "123 Fake St.",
-    fuel_types: "Vegetable oil",
-    distance: 0.75,
-    access_times: "24 hrs!"
+    {
+      station_name: "Bobs",
+      street_address: "123 Fake St.",
+      fuel_type_code: "Vegetable oil",
+      distance: 0.75,
+      access_days_time: "24 hrs!"
+    }
   }
   subject { Station.new(attrs) }
 
@@ -16,7 +18,7 @@ describe Station do
 
   it "sets attributes as reader methods" do
     expect(subject.name).to eq("Bobs")
-    expect(subject.street_address).to eq("123 Fake St.")
+    expect(subject.address).to eq("123 Fake St.")
     expect(subject.fuel_types).to eq("Vegetable oil")
     expect(subject.distance).to eq(0.75)
     expect(subject.access_times).to eq("24 hrs!")
